7bb3b53afd16d3ce7d16f7bfd13610376ca7075f
PoojaMantri311 <poojamantri1991@gmail.com>
2022-03-23 09:48:52 +0530

    Commit Code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from './components/Container';
+import React, { useEffect, useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route} from "react-router-dom";
+import ImageView from './components/ImageView';
 
-function App() {
+const App = () => {
+  const [data, setData] = useState([])
+  const [mapData, setMapData] = useState({})
+  const [loading,setLoading] = useState(true)
+
+  const convertDataIntoMap = (data) => {
+    let tempMapData = {}   
+    for(let i=0; i<data.length; i++){
+       tempMapData[data[i]['data']['id']] = data[i]['data']
+    }
+    setMapData(tempMapData)
+  }
+
+  async function getImageDataFromApi() {
+    try {
+      let response = await fetch('http://www.reddit.com/r/pics/.json?jsonp=');
+      let responseJson = await response.json();
+      setData(responseJson.data.children)
+      convertDataIntoMap(responseJson.data.children)
+      setLoading(false)
+      
+     } catch(error) {
+      console.error(error);
+    }
+  }
+  useEffect(()=>{
+    getImageDataFromApi()
+  },[])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {!loading && <Router>
+        <Routes>
+          <Route exact path="/" element={<Container data={data}/>}/>
+          <Route exact path="/:id/" element={<ImageView mapData={mapData}/>}/>
+        </Routes>
+      </Router>
+        }
     </div>
   );
 }
